b9ac951cfedce923293a411e4e4d69fa9fd97bfc
schustern <niklas.schuster@hpe.com>
2018-06-21 15:41:50 +0200

    fix imports

--- clubcorner/src/pages/home/home.ts
+++ clubcorner/src/pages/home/home.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { ModalController } from 'ionic-angular';
-import { AlertController } from 'ionic-angular';
 
-import { PlayerInviteModalPage } from '../modals/player-invite-modal/player-invite-modal';
-import { PlayerListModalPage } from '../modals/player-list-modal/player-list-modal';
-import { CreateGameModalPage } from '../modals/create-game-modal/create-game-modal';
-import { CreateTeamModalPage } from '../modals/create-team-modal/create-team-modal';
-import { CreateTrainingModalPage } from '../modals/create-training-modal/create-training-modal';
 
 import {TeamPage} from '../team/team';
 import {Services} from '../../providers/trainer/trainer';
@@ -56,3 +49,4 @@ export class HomePage {
 
 
 }
+
