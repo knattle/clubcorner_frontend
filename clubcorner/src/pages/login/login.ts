dd8e9c6a088d579447e5c887190010712c32b62b
s4dexx <nils.juenemann@outlook.com>
2018-07-05 17:56:54 +0200

    loginService done - maybe signup error

--- clubcorner/src/pages/login/login.ts
+++ clubcorner/src/pages/login/login.ts
@@ -4,6 +4,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import {HomePage} from '../home/home';
 import {login} from "../../Schema/login.schema";
 import {Services} from "../../providers/trainer/trainer";
+import {Person} from "../../Schema/person.schema";
 
 @Component({
   selector: 'page-login',
@@ -15,7 +16,17 @@ export class LoginPage {
   loginForm: any;
   icons: string[];
   items: Array<{title: string}>;
-  daten: login;
+  daten: login = {
+    loginName: null,
+    loginPw: null
+  }
+  person: Person = {
+    id: null,
+    firstname: null,
+    lastname: null,
+    loginName: null,
+    loginpw: null
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private _teamProv: Services) {
     // If we navigated to this page, we will have an item available as a nav param
@@ -26,17 +37,35 @@ export class LoginPage {
   openHomePage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.navCtrl.setRoot(HomePage);
-    //this.postData();
+    this.postLoginData();
+    this.postSignupData();
   }
 
 
- /* postData(){
+  postLoginData(){
+ console.log(this.daten)
      this._teamProv.logIn(this.daten).subscribe(
     (data) => {
+      this.navCtrl.setRoot(HomePage);
      console.log(data);
     },
-    error => console.log(error)
+    error => {console.log(error);
+      if (error.status == 401) {
+        alert("wrong credentials");
+      } else {
+        alert("something failure");
+      }
+    }
     )
-  }*/
+  }
+
+  postSignupData(){
+  console.log(this.person)
+    this._teamProv.createPerson(this.person).subscribe(
+      (data) => {
+        console.log(data);
+      },
+      error => console.log(error)
+    )
+  }
 }
